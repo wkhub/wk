78c3f9ed25a1b631294aa7ce76a4f18f348c6600
Axel Haustant <noirbizarre@gmail.com>
2018-11-25 12:45:38 +0100

    Added types.Set

---
+++ utils/types/set.go
@@ -0,0 +1,34 @@
+package types
+
+var present = struct{}{}
+
+type set struct {
+	data map[interface{}]struct{}
+}
+
+// Set is a constructor and return an actual set
+func Set(data ...interface{}) *set {
+	s := &set{}
+	s.data = make(map[interface{}]struct{})
+	for _, key := range data {
+		s.data[key] = present
+	}
+	return s
+}
+
+func (s *set) Add(key interface{}) {
+	s.data[key] = present
+}
+
+func (s set) Remove(key interface{}) {
+	delete(s.data, key)
+}
+
+func (s set) Has(key interface{}) bool {
+	_, in := s.data[key]
+	return in
+}
+
+func (s set) Length() int {
+	return len(s.data)
+}
---
+++ utils/types/set_test.go
@@ -0,0 +1,44 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetFlow(t *testing.T) {
+	set := Set()
+
+	set.Add("value")
+
+	assert.Equal(t, 1, set.Length())
+	assert.True(t, set.Has("value"))
+	assert.False(t, set.Has("missing"))
+
+	set.Add("value")
+	assert.Equal(t, 1, set.Length())
+	assert.True(t, set.Has("value"))
+
+	set.Remove("value")
+	assert.Equal(t, 0, set.Length())
+	assert.False(t, set.Has("value"))
+
+	set.Remove("value")
+	assert.Equal(t, 0, set.Length())
+	assert.False(t, set.Has("value"))
+}
+
+func TestSetConstructor(t *testing.T) {
+	set := Set("first", "second")
+
+	assert.Equal(t, 2, set.Length())
+	assert.True(t, set.Has("first"))
+	assert.True(t, set.Has("second"))
+	assert.False(t, set.Has("value"))
+
+	set.Add("value")
+	assert.Equal(t, 3, set.Length())
+	assert.True(t, set.Has("first"))
+	assert.True(t, set.Has("second"))
+	assert.True(t, set.Has("value"))
+}
