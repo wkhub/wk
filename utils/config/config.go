2925b5263eb2811ea140c123f625ace7c026b0c8
Axel Haustant <noirbizarre@gmail.com>
2018-11-25 16:23:51 +0100

    Added Config interface and cascading config support

--- utils/config/config.go
+++ utils/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"time"
+	"reflect"
 
 	"github.com/spf13/viper"
 )
@@ -17,7 +17,7 @@ type Config interface {
 	Get(key string) interface{}
 	// Sub returns new Viper instance representing a sub tree of this instance.
 	// Sub is case-insensitive for a key.
-	Sub(key string) *Config
+	Sub(key string) Config
 	// GetString returns the value associated with the key as a string.
 	GetString(key string) string
 	// GetBool returns the value associated with the key as a boolean.
@@ -28,58 +28,132 @@ type Config interface {
 	GetInt32(key string) int32
 	// GetInt64 returns the value associated with the key as an integer.
 	GetInt64(key string) int64
-	// GetFloat64 returns the value associated with the key as a float64.
-	GetFloat64(key string) float64
-	// GetTime returns the value associated with the key as time.
-	GetTime(key string) time.Time
-	// GetDuration returns the value associated with the key as a duration.
-	GetDuration(key string) time.Duration
-	// GetStringSlice returns the value associated with the key as a slice of strings.
-	GetStringSlice(key string) []string
-	// GetStringMap returns the value associated with the key as a map of interfaces.
-	GetStringMap(key string) map[string]interface{}
-	// GetStringMapString returns the value associated with the key as a map of strings.
-	GetStringMapString(key string) map[string]string
-	// GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
-	GetStringMapStringSlice(key string) map[string][]string
-	// GetSizeInBytes returns the size of the value associated with the given key
-	// in bytes.
-	GetSizeInBytes(key string) uint
-	// UnmarshalKey takes a single key and unmarshals it into a Struct.
-	UnmarshalKey(key string, rawVal interface{}, opts ...viper.DecoderConfigOption) error
-	// Unmarshal unmarshals the config into a Struct. Make sure that the tags
-	// on the fields of the structure are properly set.
-	Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error
+	// // GetFloat64 returns the value associated with the key as a float64.
+	// GetFloat64(key string) float64
+	// // GetTime returns the value associated with the key as time.
+	// GetTime(key string) time.Time
+	// // GetDuration returns the value associated with the key as a duration.
+	// GetDuration(key string) time.Duration
+	// // GetStringSlice returns the value associated with the key as a slice of strings.
+	// GetStringSlice(key string) []string
+	// // GetStringMap returns the value associated with the key as a map of interfaces.
+	// GetStringMap(key string) map[string]interface{}
+	// // GetStringMapString returns the value associated with the key as a map of strings.
+	// GetStringMapString(key string) map[string]string
+	// // GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
+	// GetStringMapStringSlice(key string) map[string][]string
+	// // GetSizeInBytes returns the size of the value associated with the given key
+	// // in bytes.
+	// GetSizeInBytes(key string) uint
+	// // UnmarshalKey takes a single key and unmarshals it into a Struct.
+	// UnmarshalKey(key string, rawVal interface{}, opts ...viper.DecoderConfigOption) error
+	// // Unmarshal unmarshals the config into a Struct. Make sure that the tags
+	// // on the fields of the structure are properly set.
+	// Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error
+	// Set sets the value for the key in the override register.
+	// Set is case-insensitive for a key.
+	// Will be used instead of values obtained via
+	// flags, config file, ENV, default, or key/value store.
+	Set(key string, value interface{})
 	// IsSet checks to see if the key has been set in any of the data locations.
 	// IsSet is case-insensitive for a key.
 	IsSet(key string) bool
-	// InConfig checks to see if the given key (or an alias) is in the config file.
-	InConfig(key string) bool
-	// SetDefault sets the default value for this key.
-	// SetDefault is case-insensitive for a key.
-	// Default only used when no value is provided by the user via flag, config or ENV.
-	SetDefault(key string, value interface{})
-	// AllKeys returns all keys holding a value, regardless of where they are set.
-	// Nested keys are returned with a v.keyDelim (= ".") separator
-	AllKeys() []string
-	// AllSettings merges all settings and returns them as a map[string]interface{}.
-	AllSettings() map[string]interface{}
+	// // InConfig checks to see if the given key (or an alias) is in the config file.
+	// InConfig(key string) bool
+	// // SetDefault sets the default value for this key.
+	// // SetDefault is case-insensitive for a key.
+	// // Default only used when no value is provided by the user via flag, config or ENV.
+	// SetDefault(key string, value interface{})
+	// // AllKeys returns all keys holding a value, regardless of where they are set.
+	// // Nested keys are returned with a v.keyDelim (= ".") separator
+	// AllKeys() []string
+	// // AllSettings merges all settings and returns them as a map[string]interface{}.
+	// AllSettings() map[string]interface{}
 }
 
-type CascadingConfig struct {
+type cascade struct {
 	configs []Config
 }
 
-func (cfg CascadingConfig) Get(key string) interface{} {
-	for _, config := range cfg.configs {
-		if config.IsSet(key) {
-			return config.Get(key)
-		}
+type config struct {
+	*viper.Viper
+}
+
+func (cfg config) Sub(key string) Config {
+	sub := cfg.Viper.Sub(key)
+	if sub != nil {
+		return config{sub}
 	}
 	return nil
 }
 
-func (cfg CascadingConfig) IsSet(key string) bool {
+// New returns a new simple Config instance
+func New() Config {
+	return config{viper.New()}
+}
+
+// Cascade returns a new cascading Config instance
+func Cascade(configs ...Config) Config {
+	return cascade{configs}
+}
+
+func callByName(cfg Config, method string, key string) interface{} {
+	meth := reflect.ValueOf(cfg).MethodByName(method)
+	params := []reflect.Value{reflect.ValueOf(key)}
+	return meth.Call(params)[0].Interface()
+}
+
+func (cfg cascade) first(method string, key string) interface{} {
+	var last Config
+	for _, config := range cfg.configs {
+		if config.IsSet(key) {
+			return callByName(config, method, key)
+		}
+		last = config
+	}
+	return callByName(last, method, key)
+}
+
+func (cfg cascade) Get(key string) interface{} {
+	return cfg.first("Get", key)
+}
+
+func (cfg cascade) Set(key string, value interface{}) {
+	cfg.configs[0].Set(key, value)
+}
+
+func (cfg cascade) Sub(key string) Config {
+	var subs []Config
+	for _, config := range cfg.configs {
+		sub := config.Sub(key)
+		if sub != nil {
+			subs = append(subs, sub)
+		}
+	}
+	return Cascade(subs...)
+}
+
+func (cfg cascade) GetString(key string) string {
+	return cfg.first("GetString", key).(string)
+}
+
+func (cfg cascade) GetBool(key string) bool {
+	return cfg.first("GetBool", key).(bool)
+}
+
+func (cfg cascade) GetInt(key string) int {
+	return cfg.first("GetInt", key).(int)
+}
+
+func (cfg cascade) GetInt32(key string) int32 {
+	return cfg.first("GetInt32", key).(int32)
+}
+
+func (cfg cascade) GetInt64(key string) int64 {
+	return cfg.first("GetInt64", key).(int64)
+}
+
+func (cfg cascade) IsSet(key string) bool {
 	for _, config := range cfg.configs {
 		if config.IsSet(key) {
 			return true
@@ -87,5 +161,3 @@ func (cfg CascadingConfig) IsSet(key string) bool {
 	}
 	return false
 }
-
-// func (cfg CascadingConfig) Sub()
---
+++ utils/config/config_test.go
@@ -0,0 +1,224 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/wkhub/wk/utils/config"
+)
+
+func TestCascadingGet(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg1.Set("key", "value1")
+	cfg2.Set("key", "value2")
+
+	cfg1.Set("key1", "value1")
+
+	cfg2.Set("key2", "value2")
+
+	// Value taken from cfg1 over cfg2
+	assert.True(t, cfg.IsSet("key"))
+	assert.Equal(t, cfg.Get("key"), "value1")
+
+	// Only present in cfg1
+	assert.True(t, cfg.IsSet("key1"))
+	assert.Equal(t, cfg.Get("key1"), "value1")
+
+	// Only present in cfg2
+	assert.True(t, cfg.IsSet("key2"))
+	assert.Equal(t, cfg.Get("key2"), "value2")
+
+	// Missing from every config
+	assert.False(t, cfg.IsSet("missing"))
+	assert.Equal(t, cfg.Get("missing"), nil)
+}
+
+func TestCascadingSet(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg.Set("key", "value")
+
+	assert.True(t, cfg.IsSet("key"))
+	assert.Equal(t, cfg.Get("key"), "value")
+
+	assert.True(t, cfg1.IsSet("key"))
+	assert.Equal(t, cfg1.Get("key"), "value")
+
+	assert.False(t, cfg2.IsSet("key"))
+	assert.Equal(t, cfg2.Get("key"), nil)
+}
+
+func TestCascadingSub(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+	cfg3 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2, cfg3)
+
+	cfg1.Set("root.key", "value1")
+	cfg2.Set("root.key", "value2")
+
+	cfg1.Set("root.key1", "value1")
+
+	cfg2.Set("root.key2", "value2")
+
+	cfg3.Set("outside.missing", "value")
+
+	sub := cfg.Sub("root")
+
+	// Value taken from cfg1 over cfg2
+	assert.True(t, sub.IsSet("key"))
+	assert.Equal(t, sub.Get("key"), "value1")
+
+	// Only present in sub1
+	assert.True(t, sub.IsSet("key1"))
+	assert.Equal(t, sub.Get("key1"), "value1")
+
+	// Only present in sub2
+	assert.True(t, sub.IsSet("key2"))
+	assert.Equal(t, sub.Get("key2"), "value2")
+
+	// Missing from every config
+	for _, key := range []string{"outside.missing", "outside", "missing"} {
+		assert.False(t, sub.IsSet(key))
+		assert.Equal(t, sub.Get(key), nil)
+	}
+}
+
+func TestCascadingGetString(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg1.Set("key", "value1")
+	cfg2.Set("key", "value2")
+
+	cfg1.Set("key1", "value1")
+
+	cfg2.Set("key2", "value2")
+
+	// Value taken from cfg1 over cfg2
+	assert.Equal(t, cfg.GetString("key"), "value1")
+
+	// Only present in cfg1
+	assert.Equal(t, cfg.GetString("key1"), "value1")
+
+	// Only present in cfg2
+	assert.Equal(t, cfg.GetString("key2"), "value2")
+
+	// Missing from every config
+	assert.Equal(t, cfg.GetString("missing"), "")
+}
+
+func TestCascadingGetBool(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg1.Set("key", true)
+	cfg2.Set("key", false)
+
+	cfg1.Set("key1", true)
+
+	cfg2.Set("key2", false)
+
+	// Value taken from cfg1 over cfg2
+	assert.True(t, cfg.GetBool("key"))
+
+	// Only present in cfg1
+	assert.True(t, cfg.GetBool("key1"))
+
+	// Only present in cfg2
+	assert.False(t, cfg.GetBool("key2"))
+
+	// Missing from every config
+	assert.False(t, cfg.GetBool("missing"))
+}
+
+func TestCascadingGetInt(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg1.Set("key", 1)
+	cfg2.Set("key", 2)
+
+	cfg1.Set("key1", 1)
+
+	cfg2.Set("key2", 2)
+
+	// Value taken from cfg1 over cfg2
+	assert.Equal(t, cfg.GetInt("key"), 1)
+
+	// Only present in cfg1
+	assert.Equal(t, cfg.GetInt("key1"), 1)
+
+	// Only present in cfg2
+	assert.Equal(t, cfg.GetInt("key2"), 2)
+
+	// Missing from every config
+	assert.Equal(t, cfg.GetInt("missing"), 0)
+}
+
+func TestCascadingGetInt32(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg1.Set("key", 1)
+	cfg2.Set("key", 2)
+
+	cfg1.Set("key1", 1)
+
+	cfg2.Set("key2", 2)
+
+	// Value taken from cfg1 over cfg2
+	assert.Equal(t, cfg.GetInt32("key"), int32(1))
+
+	// Only present in cfg1
+	assert.Equal(t, cfg.GetInt32("key1"), int32(1))
+
+	// Only present in cfg2
+	assert.Equal(t, cfg.GetInt32("key2"), int32(2))
+
+	// Missing from every config
+	assert.Equal(t, cfg.GetInt32("missing"), int32(0))
+}
+
+func TestCascadingGetInt64(t *testing.T) {
+	cfg1 := config.New()
+	cfg2 := config.New()
+
+	cfg := config.Cascade(cfg1, cfg2)
+
+	cfg1.Set("key", 1)
+	cfg2.Set("key", 2)
+
+	cfg1.Set("key1", 1)
+
+	cfg2.Set("key2", 2)
+
+	// Value taken from cfg1 over cfg2
+	assert.Equal(t, cfg.GetInt64("key"), int64(1))
+
+	// Only present in cfg1
+	assert.Equal(t, cfg.GetInt64("key1"), int64(1))
+
+	// Only present in cfg2
+	assert.Equal(t, cfg.GetInt64("key2"), int64(2))
+
+	// Missing from every config
+	assert.Equal(t, cfg.GetInt64("missing"), int64(0))
+}
